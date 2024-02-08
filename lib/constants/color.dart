7f3a2509d5f4cb1bd2d2be3e0d8ee93a575f3cfb
Dovletyar <dovletyar4@gmail.com>
2024-02-08 18:02:36 +0300

    TextField a prefixIcon(Search.svg), suffixIcon(Filter.svg), aralarına ise VerticalDivider çizgisi eklendi, ayarları yapıldı

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,2 +1,4 @@
 const String secondaryColor = "F7F8F8";
 const String shadowColor = "1D1617";
+const String dividerColor = "DDDADA";
+const String blackColor = "FFFFFF";
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -26,6 +26,27 @@ class HomePage extends StatelessWidget {
                 filled: true,
                 fillColor: Colors.white,
                 contentPadding: const EdgeInsets.all(15),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset("assets/icons/Search.svg"),
+                ),
+                suffixIcon: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset("assets/icons/Filter.svg"),
+                      ),
+                    ],
+                  ),
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(15),
                   borderSide: BorderSide.none,
